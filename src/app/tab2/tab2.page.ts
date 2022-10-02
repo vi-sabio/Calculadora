b46bac61bfc9511e8faf686776b77b017c3f1581
vi-sabio <vinicios_sabio@hotmail.com>
2022-10-02 19:31:05 -0300

    calculadora
    calculadora

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+
+  operacao = '';
+  resultado = '';
+
 
   constructor() {}
 
