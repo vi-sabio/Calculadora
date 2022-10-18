efab9f8305e305687ac539718304f6b18f774bec
vi-sabio <vinicios_sabio@hotmail.com>
2022-10-17 22:26:30 -0300

    calculadora

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,48 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { evaluate } from 'mathjs';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
   operacao = '';
   resultado = '';
   ultimovalor = '';
+  numero = false;
+  caracter = true;
+  caracteres = ['.','/','*','+','-'];
 
 
   constructor() {}
+
+  ngOnInit() {}
+
+  calcularOperacao(){
+    try{
+      this.resultado = evaluate(this.operacao);
+    } catch (err) {
+    this.resultado = 'Inválido!';
+    }
+  }
   
   adicionarValor(valor:string){
-    if(this.operacao !=="){
+    this.caracter = this.caracteres.includes(valor);
 
-      this.ultimoValor= this.operacao[this.operacao.length-1];
+    if (!this.caracter) {
+      this.operacao += valor;
+      this.numero = true;
+    } else if (this.caracter && this.numero) {
+      this.operacao += valor;
+      this.numero = false;
     }
-    if(valor)
+  }
+
+  limparMemoria() {
+    this.operacao = '';
+    this.resultado = '';
+    this.numero = false;
   }
   
-
-  adicionarValor(valor: string) {
-    this.operacao += valor;
-  }
 
   limparOperacao(){
     this.operacao = '';
-
-  }
-
-  limparMemoria(){
-    this.operacao = '';
-    this.resultado = '';
-
+    this.numero = false;
 
   }
 
   apagarCaracter(){
     if (this.operacao.length > 0){
     this.operacao = this.operacao.substring(0, this.operacao.length - 1);
+    }
+
+    const ultimo = this.operacao.substring(this.operacao.length, 1);
+    this.caracter = this.caracteres.includes(ultimo);
+
+    console.log(ultimo);
+
+    if (!this.caracter) {
+      this.numero = true;
+    } else {
+      this.numero = false;
     }
   }
 }
